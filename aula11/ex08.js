3c695563a1cd7e2fc6b3e95b045f269064e0ae31
victoria <148492863+V1ctor1aTorres@users.noreply.github.com>
2023-11-10 22:08:03 -0300

    criei ex08 e ex09

---
+++ aula11/ex08.js
@@ -0,0 +1,7 @@
+var vel = 60.5
+console.log(`A velocidade é ${vel}Km/h.`)
+if(vel >60) {
+    console.log(`Você ultrapassou a velocidade permitida. Multado!`)
+}
+    console.log(`Dirija sempre usando cinto de segurança!`)
+
