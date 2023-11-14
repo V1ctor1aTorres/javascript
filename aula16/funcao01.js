55269dfd56a2ee2553e2d3b237a669fdfe2b95cb
victoria <148492863+V1ctor1aTorres@users.noreply.github.com>
2023-11-14 15:21:52 -0300

    atualização

---
+++ aula16/funcao01.js
@@ -0,0 +1,12 @@
+function parImpar(n) {
+    if(n%2 == 0) {
+        return 'par'
+    } else {
+        return 'impar'
+    }
+}
+
+//let res = parImpar(4)
+//console.log(res)
+
+console.log(parImpar(4))
